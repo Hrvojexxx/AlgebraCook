e28d6988213dc7a98b07b77d688c906158165354
Hrvoje Mirković <hrvoje.mirkovic@predavaci.algebra.hr>
2017-11-08 20:07:58 +0100

    napravjen Recipes Controller

---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class RecipesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return ("Popis svih recepata");
+    }
+
+    public function add()
+    {
+        return ("prikaz view-a s web obrascem za unos");
+    }
+	
+	public function save(Request $request) 
+	{
+		//$request->nameInputa = vrijednostInputa
+		return ("Sprema podatke iz WebObrasca u bazu");
+	}
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+   
+    public function view($id)
+    {
+        //prikaz recepta s određenim $id
+		return "Prikaz recepta ID: " . $id;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Edit recepta sa ID: " . $id; //Prikaz web obrasca
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return "Spremanje promjena recepta ID: " . $id;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete($id)
+    {
+        return "Brisanje recepta ID: " . $id;
+    }
+}
