9ec9a43f94d9a97f552db278dcd07c32254be29d
Hrvoje Mirković <hrvoje.mirkovic@predavaci.algebra.hr>
2017-11-12 12:00:18 +0100

    master, index i error view dodani

---
+++ resources/views/index.blade wrong url.php
@@ -0,0 +1,18 @@
+@extends('master')
+
+@section('ttle', 'Recepti')
+
+@section('content')
+<div class="container">
+	<div class="col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel-heading">Recepti<br>
+				<a href="http://localhost/recepies/add">
+					<i class="fa fa-btn fa-plus"></i>
+					Dodaj novi recept
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
